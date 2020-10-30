6d85e804e783144ae05af431dd034bac83762ca3
Sean Fahy <sean@humanlambdas.com>
2020-10-30 10:56:27 +0100

    added quicksort code

---
+++ quicksort.py
@@ -0,0 +1,17 @@
+def quicksort(array = [9,8,7,-6,5.2,6,2,7,-8,4,6,3,7,2,9,5,8,0,-4,3,3,2,1]):
+    if len(array) < 1:
+        return array
+
+    pivotValue, leftArray, equalArray, rightArray = array[len(array) // 2], [], [], []
+    
+    for value in array:
+        if value < pivotValue:
+            leftArray.append(value)
+        elif value == pivotValue:
+            equalArray.append(value)
+        else:
+            rightArray.append(value)
+
+    return quicksort(leftArray) + equalArray + quicksort(rightArray)
+
+print(quicksort())
