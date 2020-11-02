2f631c544f27ac08fac51df291ea33ab6b5ef6f5
Sean Fahy <sean@humanlambdas.com>
2020-11-02 04:06:53 +0100

    added bubble sort code

---
+++ bubblesort.py
@@ -0,0 +1,13 @@
+def bubblesort(array = [4,1,3,5]):
+    arrayLength = len(array)
+
+    for i in range(arrayLength - 1):
+
+        for j in range(0, arrayLength - 1 - i):
+
+            if array[j] > array[j + 1]:
+                array[j + 1], array[j] = array[j], array[j + 1]
+
+    return array
+
+print(bubblesort())
