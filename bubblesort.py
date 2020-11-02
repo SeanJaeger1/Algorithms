0816cd99b6e6cce8410adc429ca1386d1414b151
Sean Fahy <sean@humanlambdas.com>
2020-11-02 04:12:05 +0100

    removed redundant 0 start in range

--- bubblesort.py
+++ bubblesort.py
@@ -3,7 +3,7 @@ def bubblesort(array = [4,1,3,5]):
 
     for i in range(arrayLength - 1):
 
-        for j in range(0, arrayLength - 1 - i):
+        for j in range(arrayLength - 1 - i):
 
             if array[j] > array[j + 1]:
                 array[j + 1], array[j] = array[j], array[j + 1]
