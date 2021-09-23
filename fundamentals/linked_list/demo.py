28ab2db50887e3202e9e0ad65714a4573530382d
Sean Jaeger <seanljaeger@gmail.com>
2021-09-23 23:00:54 +0200

    Added demo script, pop and push front

---
+++ fundamentals/linked_list/demo.py
@@ -0,0 +1,14 @@
+from linked_list import LinkedList
+
+
+example_list = LinkedList()
+
+print(example_list)
+example_list.push_front(1)
+print(example_list)
+example_list.push_back(5)
+print(example_list)
+example_list.push_front(7)
+print(example_list)
+example_list.pop_front()
+print(example_list)
--- fundamentals/linked_list/linked_list.py
+++ fundamentals/linked_list/linked_list.py
@@ -1,32 +1,58 @@
 from node import Node
 
+
 class LinkedList():
     def __init__(self):
         self._head = None
         self._tail = None
 
-    def push_front(self, value):
+    def is_empty(self):
+        if self._head is None:
+            return true
+        else:
+            return false
+
+    def push_back(self, value):
         if self._head is None:
             inital_node = Node(value)
             self._head, self._tail = inital_node, inital_node
         else:
-            new_node = Node(value, next_node=self._head)
-            self._head = new_node
+            new_node = Node(value)
+            self._tail.next_node = new_node
+            self._tail = new_node
+
+    def pop_front(self):
+        if self._head is None:
+            raise IndexError("This list is currently empty")
+        else:
+            pop_value = self._head.value
+            if self._head.next_node:
+                self._head = self._head.next_node
+            else:
+                self._head = None
+            return pop_value
+
+    def push_front(self, value):
+        new_node = Node(value, self._head)
+        self._head = new_node
+
+        if self._tail is None:
+            self._tail = new_node
 
     def __repr__(self):
         if self._head is None:
             return "Empty linked list"
 
-        current_node = self._head
+        current_node = self._head.next_node
         output = str(self._head.value)
 
-        while current_node.next_node:
-            output = output + " -> " + current_node.value
+        while current_node:
+            output = output + " -> " + str(current_node.value)
             current_node = current_node.next_node
 
         return output
 
-    def size(self):
+    def __len__(self):
         length = 0
         current_node = self._head
 
