84b36bc23e71a1fca7d0594befdaf2f0f322b5be
Sean Jaeger <seanljaeger@gmail.com>
2021-10-03 13:41:55 +0200

    added scaffolding for dict implementation

---
+++ data_structures/dictionary/dictionary.py
@@ -0,0 +1,17 @@
+class Dictionary:
+    def add(self, key, value):
+        # if key exists, update value
+        pass
+
+    def hash(self, k, m):
+        # m is the side of the hash table
+        pass
+
+    def exists(self, key):
+        pass
+
+    def get(self, key):
+        pass
+
+    def remove(self, key):
+        pass
