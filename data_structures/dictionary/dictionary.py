dff7a02a6631289f0d1aef8cb0cfeb2e08c87f15
Sean Jaeger <seanljaeger@gmail.com>
2021-10-04 19:21:20 +0200

    Added primitive hash to index function, added resize function with load factor

--- data_structures/dictionary/dictionary.py
+++ data_structures/dictionary/dictionary.py
@@ -1,17 +1,58 @@
+import sys
+import math
+
+
 class Dictionary:
-    def add(self, key, value):
-        # if key exists, update value
+    def __init__(self):
+        self.bucket_count = 7
+        self.item_count = 0
+        self.max_load = 0.4
+        self.table = [[] for bucket in range(self.bucket_count)]
         pass
 
-    def hash(self, k, m):
-        # m is the side of the hash table
-        pass
+    def __repr__(self):
+        return str(self.table)
+
+    def _resize(self):
+        new_table = [[] for bucket in range(self.bucket_count * 2)]
+        self.bucket_count = self.bucket_count * 2
+
+        for bucket in self.table:
+            for item in bucket:
+                new_table[self._hash_to_index(item)].append(item)
+
+        self.table = new_table
+
+    def add(self, key, value):
+        new_item = [key, value]
+        self.item_count = self.item_count + 1
+
+        if self.item_count > self.bucket_count * self.max_load:
+            self._resize()
+
+        self.table[self._hash_to_index(new_item)].append(new_item)
+
+        # account for duplicate items
+
+    def _hash_to_index(self, pair):
+        position = math.floor(
+            self.bucket_count * abs(hash(pair[0])) / 2 ** (sys.hash_info.width - 1)
+        )
+        return position
 
     def exists(self, key):
-        pass
+        
 
     def get(self, key):
         pass
 
     def remove(self, key):
         pass
+
+
+k = Dictionary()
+
+print(k)
+for i in range(20):
+    k.add("item-#{0}".format(i), i ** 2)
+print(k)
