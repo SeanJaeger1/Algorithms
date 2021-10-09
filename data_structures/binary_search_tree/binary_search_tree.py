00ab2b96fc06e0d8d7cdf95c9f6f4d57151c1c69
Sean Jaeger <seanljaeger@gmail.com>
2021-10-09 22:10:16 +0200

    Added more methods

--- data_structures/binary_search_tree/binary_search_tree.py
+++ data_structures/binary_search_tree/binary_search_tree.py
@@ -7,3 +7,30 @@ class BinarySearchTree:
 
     def __repr__(self):
         pass
+
+    def insert(self, value):
+        # need a subfunction to get the specific value's parent
+        pass
+
+    def delete_tree(self):
+        self.root_node = BinarySearchTreeNode()
+
+    def get_min(self):
+        minimum = self.root_node.value
+        current_node = self.root_node.left_node
+
+        while current_node is not None:
+            minimum = current_node.value
+            current_node = current_node.left_node
+
+        return minimum
+
+    def get_max(self):
+        maximum = self.root_node.value
+        current_node = self.root_node.right_node
+
+        while current_node is not None:
+            maximum = current_node.value
+            current_node = current_node.right_node
+
+        return maximum
--- data_structures/binary_search_tree/binary_search_tree_node.py
+++ data_structures/binary_search_tree/binary_search_tree_node.py
@@ -1,7 +1,10 @@
 class BinarySearchTreeNode:
-    def __init__(self, left_node, right_node):
+    def __init__(self, value, left_node, right_node):
+        self.value = value
         self.left_node = left_node
         self.right_node = right_node
 
     def __repr__(self):
-        return "Left node is: {0}, right node is: {1}".format(self.left_node, self.right_node)
+        return "Node value is {0}, left node is: {0}, right node is: {1}".format(
+            self.value, self.left_node, self.right_node
+        )
